c728db6f97c701425dd28d33c0568bd1679f7701
Larry Botha <larry@fixate.it>
2013-10-16 00:58:16 +0200

    use CDN jquery

--- partials/scripts.inc.php
+++ partials/scripts.inc.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
  * Partial for outputting scripts to footer.inc.php
  *
  */
 include('./partials/google-analytics.inc.php'); ?>
 
-<script type="text/javascript" src="<?php echo $config->urls->templates?>assets/js/lib/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
 <script type="text/javascript" src="<?php echo $config->urls->templates?>assets/js/main.js"></script>
