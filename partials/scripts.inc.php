35e458edfafc450eadb17faf6ef10901d4a12505
Larry Botha <larry@fixate.it>
2013-11-05 18:31:02 +0200

    update jquery version

--- partials/scripts.inc.php
+++ partials/scripts.inc.php
@@ -5,6 +5,6 @@
  */
 include('./partials/google-analytics.inc.php'); ?>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 
 <script type="text/javascript" src="<?php echo $config->urls->templates?>assets/js/main.js"></script>
