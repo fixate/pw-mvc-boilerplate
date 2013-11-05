36a91bbcc8fb3685f86af899ad047278e38b9615
Larry Botha <larry@fixate.it>
2013-11-06 01:08:42 +0200

    prevent splitting of divs

--- main.php
+++ main.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Base template for delegating which view to use to render content.
  *
@@ -12,15 +11,37 @@
  * @package ProcessWire
  * @since Theme_Name 1.0
  */
+?>
+<!doctype html>
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="<?= __('en', 'theme_text_domain'); ?>"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9 oldie" lang="<?= __('en', 'theme_text_domain'); ?>"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="<?= __('en', 'theme_text_domain'); ?>"> <!--<![endif]-->
+<?php // get <head> ?>
+<?php include("./partials/meta.inc.php"); ?>
 
-include("./partials/head.inc.php");
+<body>
+  <?php // get site header ?>
+  <?php include("./partials/header.inc.php"); ?>
 
-	if( $page->template ) {
-		$t = new TemplateFile($config->paths->templates . "views/{$page->template}.inc.php");
-		//$t->set("arr1", $somevar);
-		echo $t->render();
-	}
+  <div class="area-content">
 
-include("./partials/footer.inc.php");
+    <div id="main" class="wrap" role="main">
 
+      <h1 id='title'><?= $page->get("headline|title"); ?></h1>
 
+      <?php // get template-specific content ?>
+      <?php
+        if( $page->template ) {
+          $t = new TemplateFile($config->paths->templates . "views/{$page->template}.inc.php");
+          //$t->set("arr1", $somevar);
+          echo $t->render();
+        }
+      ?>
+
+    </div><?php // #main ?>
+  </div><?php //.area-content ?>
+
+  <?php include("./partials/footer.inc.php"); ?>
+
+</body>
+</html>
