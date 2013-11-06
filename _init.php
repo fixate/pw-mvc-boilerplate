56801c911d35c931430e5bdf48e98937eecfb815
Larry Botha <larry@fixate.it>
2013-11-06 14:54:31 +0200

    move variables to controller, selectively include controller

--- _init.php
+++ _init.php
@@ -1,36 +1,35 @@
 <?php
-
-/***************************************************************************************
+/*------------------------------------*\
+  $_INIT.PHP
+\*------------------------------------*/
+/**
  * This is the global init file included before all template files.
  *
  * Use of this is optional and set via $config->prependTemplateFile in /site/config.php.
- * We are using this init file to define shared functions and variables.
- * See _out.php for the main markup file where everything is output.
- *
+ * This file is used to make controllers available to each template. Controllers are
+ * used to keep logic out of the templates, and to make available all fields defined
+ * in the templates.
  */
 
-/***************************************************************************************
- * SHARED VARIABLES
+
+
+
+
+/*------------------------------------*\
+  $CONTROLLERS
+\*------------------------------------*/
+/**
+ * Site-wide controllers which hold logic, and helpers for access to fields defined
+ * in templates.
  *
- * These are the variables we've decided template files may choose to populate.
- * These variables are ultimately output by _out.php. Here we are establishing
- * default values for them.
+ * A global controller is available to all templates. Template-specific controllers
+ * may override variables defined in the global controller.
  *
+ * Template-specific controllers are only loaded on pages using that particular
+ * template.
  */
+include "./controllers/globals-controller.php";
 
-$browserTitle = $page->title; 	// what appears in the <title> tag
-$headline = $page->title; 	// primary h1 headline
-$content = $page->body; 	// bodycopy area
-
-// if the current page number is > 1, append that in the browserTitle
-if($input->pageNum > 1) $browserTitle .= " - Page {$input->pageNum}";
-
-
-/***************************************************************************************
- * SHARED FUNCTIONS
- *
- * We include a set of shared functions here so that they can be used by any of our
- * template files as needed.
- *
- */
-include("./functions.php");
+if ($page->template) {
+  include "./controllers/{$page->template}-controller.php";
+}
