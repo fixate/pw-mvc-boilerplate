ff2a6d598ed5349df4ff43099550aa9323aaa04a
Larry Botha <larry@fixate.it>
2013-11-06 14:56:47 +0200

    use variable helper

--- partials/scripts.inc.php
+++ partials/scripts.inc.php
@@ -10,4 +10,4 @@ get_google_analytics(); ?>
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 
-<script type="text/javascript" src="<?php echo $config->urls->templates?>assets/js/main.js"></script>
+<script type="text/javascript" src="<?= $assets_uri; ?>/js/main.js"></script>
