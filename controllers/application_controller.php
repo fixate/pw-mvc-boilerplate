e460658edc71944fd3f1ecc15ad29aae2bc96bf8
Stan <sdbondi@gmail.com>
2014-01-31 10:13:22 +0200

    Added CDN to javascript trait

--- controllers/application_controller.php
+++ controllers/application_controller.php
@@ -3,23 +3,29 @@
 \fixate\Php::require_all(TEMPLATE_DIR.'/controllers/traits');
 
 class ApplicationController extends Controller {
-  use JavaScript;
+	use Javascript;
 	use SEO;
 	use VideoEmbed;
 	use PrimaryNav;
-	use Search;
+	use Utils;
 
 	function initialize() {
-    JavaScript::__jsInitialize($this);
+		Javascript::__jsInitialize($this);
 		SEO::__seoInitialize($this);
 		VideoEmbed::__vidembedInitialize($this);
 		PrimaryNav::__pnInitialize($this);
-		Search::__searchInitialize($this);
+		Utils::__utilsInitialize($this);
+
+		$this->js_add_cdn(
+			'//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js',
+			'window.jQuery',
+			'vendor/jquery/jquery.js'
+		);
 	}
 
   // Fallback index
   function index() {
-    return $this->render($this->config->page->template->name);
+		return $this->render($this->config->page->template->name);
   }
 }
 
--- controllers/traits/javascript.php
+++ controllers/traits/javascript.php
@@ -4,41 +4,54 @@ trait Javascript {
 	static function __jsInitialize($obj) {
 		$obj->helper('js_add_vendor');
 		$obj->helper('js_add_script');
+		$obj->helper('js_add_cdn');
 		$obj->helper('render_scripts');
-		$obj->helper('vendor_scripts');
 	}
 
-	private $__js_vendor = array();
-	private $__js_user = array();
+	private $__js_scripts = array();
 
-	function js_add_vendor($script) {
-		$this->__js_vendor[] = $script;
+	function js_add_vendor($script, $path_tmpl = 'vendor/%%/%%.js') {
+		$this->__js_scripts[] = array('vendor', $script, $path_tmpl);
 	}
 
-	function js_add_script($script) {
-		$this->__js_user[] = $script;
+	function js_add_script($script, $path_tmpl = 'js/%%') {
+		$this->__js_scripts[] = array('user', $script, $path_tmpl);
 	}
 
-	function vendor_scripts() {
-		return $this->__js_render($this->__js_vendor, 'vendor/%%/%%.js');
+	function js_add_cdn($url, $detect = false, $fallback = false) {
+		$this->__js_scripts[] = array('cdn', $url, array($detect, $fallback));
 	}
 
 	function render_scripts() {
-		return $this->__js_render($this->__js_user, 'js/%%.js');
-	}
-
-	private function __js_render($scripts, $root = '') {
 		$html = '';
-
-		foreach ($scripts as $script) {
-			$path = str_replace('%%', $script, $root);
-			$html .= $this->__js_tag($this->view->assets($path));
+		foreach ($this->__js_scripts as $script) {
+			list($type, $url) = $script;
+			switch ($type) {
+			case 'user':
+			case 'vendor':
+				$path = $this->view->assets(str_replace('%%', $url, array_pop($script)));
+				$html .= $this->__script_tag($path);
+				break;
+			case 'cdn':
+				$html .= $this->__cdn_tag($url, array_pop($script));
+				break;
+			}
 		}
 
 		return $html;
 	}
 
-	private function __js_tag($src) {
+	private function __cdn_tag($url, $fallback = false) {
+		$html = $this->__script_tag($url);
+		if ($fallback) {
+			list($detect, $asset) = $fallback;
+			$asset = $this->view->assets($asset);
+			$html .= "<script>({$detect}) || document.write('<script src=\"{$asset}\">\\x3C/script>')</script>";
+		}
+		return $html;
+	}
+
+	private function __script_tag($src) {
 		return "<script type=\"text/javascript\" src=\"{$src}\"></script>";
 	}
 }
