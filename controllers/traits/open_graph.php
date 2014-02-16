ac9bdd532b26e3194c53698310a90f77cc14c87d
Larry Botha <larry@fixate.it>
2014-02-16 17:05:22 +0200

    correctly handle multiple values of an opengraph tag

--- controllers/traits/open_graph.php
+++ controllers/traits/open_graph.php
@@ -6,7 +6,7 @@ trait OpenGraph {
 	}
 
 	private $__og_opts = array(
-		'image_get' => 'thumbnail|image|images',
+		'image_get' => 'image|images',
 		'title_get' => 'title',
 		'canonical_url' => false,
 		'object_type' => false,
@@ -19,27 +19,29 @@ trait OpenGraph {
 	}
 
 	function opengraph_meta_tags() {
-		$image = $this->__og_get_prop('image');
+		$image = $this->__og_get_prop('image', 'url');
 		$tags = array(
 			'site_name' => $this->__og_opts['site_name'],
 			'type' => $this->__og_opts['object_type'] ? $this->__og_opts['object_type'] : $this->__og_opts['object_type_default'],
 			'title' => $this->__og_get_prop('title'),
 			'url'   => $this->__og_opts['canonical_url'] ? $this->__og_opts['canonical_url'] : $this->page->httpUrl,
-			'image' => $image ? $image->url : null
+			'image' => $this->__og_image_meta($image)
 		);
 
 		return $this->__og_render_tags($tags);
 	}
 
-	private function __og_get_prop($prop, $default = null) {
+	private function __og_get_prop($prop, $field = null, $default = null) {
 		$prop = $this->page->get($this->__og_opts["{$prop}_get"]);
 
 		if ($prop && method_exists($prop, 'count')) {
 			if ($prop->count() == 0) {
 				return $default;
+			} elseif ($prop->count() > 1) {
+				$prop = $this->__og_get_prop_array($prop, $field);
+			} else {
+				$prop = $prop->first();
 			}
-
-			$prop = $prop->first();
 		}
 
 		if (!$prop) {
@@ -49,13 +51,45 @@ trait OpenGraph {
 		return $prop;
 	}
 
+	private function __og_get_prop_array($prop, $field) {
+		$prop_array = [];
+
+		foreach ($prop as $p) {
+			array_push($prop_array, $p->$field);
+		}
+
+		return $prop_array;
+	}
+
+	private function __og_image_meta($image) {
+		if ($image) {
+			if (is_array($image)) {
+				return $image;
+			}
+
+			return $image->url;
+		}
+		return null;
+	}
+
 	private function __og_render_tags($tags) {
 		$html = '';
 		foreach ($tags as $name => $value) {
-			if ($value !== null) {
-				$html .= "<meta property='og:{$name}' content='{$value}'/>";
+			if ($value !== null && !is_array($value)) {
+				$html .= $this->__og_tag_markup($name, $value);
+			}
+
+			if (is_array($value)) {
+				foreach ($value as $v) {
+					$html .= $this->__og_tag_markup($name, $v);
+				}
 			}
 		}
 		return $html;
 	}
+
+	private function __og_tag_markup($name, $value) {
+		return "<meta property='og:{$name}' content='{$value}'/>";
+	}
 }
+
