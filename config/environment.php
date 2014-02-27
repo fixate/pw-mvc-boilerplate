d5bcaac4e917b5f1d97ab91ef642d87990d8d7d8
Larry Botha <larry@fixate.it>
2014-02-27 12:18:03 +0200

    ensure we're not setting the environment explicitly

--- config/environment.php
+++ config/environment.php
@@ -5,7 +5,7 @@
 $ga_uacode = $pages->get('/settings')->ga_uacode ? $pages->get('/settings')->ga_uacode : false;
 
 $environment = array(
-'env' => 'production',
+	// 'env' => 'production',
 
 	// Google analytics
 	'ga_uacode' => $ga_uacode
