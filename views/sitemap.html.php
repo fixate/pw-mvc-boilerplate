3bf72d3c23ea990696875ca29d7ecdeccd4275a2
Larry Botha <larry@fixate.it>
2014-03-31 17:15:34 +0200

    remove comments about delegate approach

--- views/sitemap.html.php
+++ views/sitemap.html.php
@@ -2,9 +2,6 @@
 /**
  * Sitemap template
  *
- * This site uses the delegate approach:
- * http://processwire.com/talk/topic/740-a-different-way-of-using-templates-delegate-approach/
- *
  * Make sure to set 'Alternate Template' to 'mvc' under Template Settings
  *
  * @package ProcessWire
