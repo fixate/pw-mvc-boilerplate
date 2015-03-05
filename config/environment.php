40632e50e400888099044145814ba263dfe7b28d
Larry Botha <larry@fixate.it>
2015-03-05 13:26:42 +0200

    add webmaster tools to environment

--- config/environment.php
+++ config/environment.php
@@ -5,12 +5,14 @@ use fixate as f8;
 	USER ENVIRONMENT
 \*------------------------------------*/
 $ga_uacode = $pages->get('/settings')->ga_uacode ? $pages->get('/settings')->ga_uacode : false;
+$ga_wmt_meta = $pages->get('/settings')->ga_wmt_meta ? $pages->get('/settings')->ga_wmt_meta : false;
 
 $environment = array(
 	// 'env' => 'production',
 
 	// Google analytics
 	'ga_uacode' => $ga_uacode,
+	'ga_wmt_meta' => $ga_wmt_meta,
 	// Asset Manifest
 	'use_manifest' => true
 );
