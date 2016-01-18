1ec7f2a4fda76759acd30703eebb5728a0bd510a
Larry Botha <larry@fixate.it>
2016-01-18 10:22:06 +0200

    use js instead of coffee

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
-(function() {
+/*--------------------------------------------------*\
+  MAIN
+\*--------------------------------------------------*/
+import toggler from './lib/toggler';
 
+var body = document.body;
 
-}).call(this);
+if ($.fn.toggler) {
+  $('.js-menu-toggle').toggler({
+    activeClass: 'is-open-nav'
+  });
+}
+
+$(window).on('scroll', function() {
+  var timer = void 0;
+  clearTimeout(timer);
+
+  if (!body.classList.contains("disable-hover")) {
+    body.classList.add("disable-hover");
+  }
+
+  timer = setTimeout(function() {
+    body.classList.remove("disable-hover");
+  }, 50);
+});
+
+$('html').removeClass('no-js');
