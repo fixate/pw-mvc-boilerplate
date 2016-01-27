f18271337d77789d30504f919efa9013d0c2d2b7
Larry Botha <larry@fixate.it>
2016-01-27 15:36:35 +0200

    use es6 and update linting of main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,28 @@
-/*--------------------------------------------------*\
+/* -------------------------------------------------- *\
     MAIN
-\*--------------------------------------------------*/
+\* -------------------------------------------------- */
 import toggler from './lib/toggler';
 
-var body = document.body;
+const $ = window.jQuery;
 
 if ($.fn.toggler) {
   $('.js-menu-toggle').toggler({
-    activeClass: 'is-open-nav'
+    activeClass: 'is-open-nav',
   });
 }
 
-$(window).on('scroll', function() {
-  var timer = void 0;
+window.addEventListener('scroll', () => {
+  const body = document.body;
+  let timer = void 0;
   clearTimeout(timer);
 
-  if (!body.classList.contains("disable-hover")) {
-    body.classList.add("disable-hover");
+  if (!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover');
   }
 
-  timer = setTimeout(function() {
-    body.classList.remove("disable-hover");
-  }, 50);
+  timer = setTimeout(() => {
+    body.classList.remove('disable-hover');
+  }, 100);
 });
 
 $('html').removeClass('no-js');
