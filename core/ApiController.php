76a74124ea109d3f7e3e612bf2ecebb459203001
Stan Bondi <sdbondi@users.noreply.github.com>
2016-03-08 17:35:40 +0200

    Indentation + fixed incorrect exception class name

--- core/ApiController.php
+++ core/ApiController.php
@@ -4,219 +4,219 @@ use fixate as f8;
 
 abstract class ApiController implements IController
 {
-    public $config = null;
-    public $fields = null;
-    public $input = null;
-    public $page = null;
-    public $pages = null;
-    public $permissions = null;
-    public $roles = null;
-    public $sanitizer = null;
-    public $session = null;
-    public $templates = null;
-    public $user = null;
-    public $users = null;
+  public $config = null;
+  public $fields = null;
+  public $input = null;
+  public $page = null;
+  public $pages = null;
+  public $permissions = null;
+  public $roles = null;
+  public $sanitizer = null;
+  public $session = null;
+  public $templates = null;
+  public $user = null;
+  public $users = null;
 
-    protected $request = null;
-    protected $response = null;
+  protected $request = null;
+  protected $response = null;
 
-    public function __construct(&$config, &$fields, &$input, &$page, &$pages, &$permissions, &$roles, &$sanitizer, &$session, &$templates, &$user, &$users)
-    {
-        $this->config = $config;
-        $this->fields = $fields;
-        $this->input = $input;
-        $this->page = $page;
-        $this->pages = $pages;
-        $this->permissions = $permissions;
-        $this->roles = $roles;
-        $this->sanitizer = $sanitizer;
-        $this->session = $session;
-        $this->templates = $templates;
-        $this->user = $user;
-        $this->users = $users;
-    }
+  public function __construct(&$config, &$fields, &$input, &$page, &$pages, &$permissions, &$roles, &$sanitizer, &$session, &$templates, &$user, &$users)
+  {
+    $this->config = $config;
+    $this->fields = $fields;
+    $this->input = $input;
+    $this->page = $page;
+    $this->pages = $pages;
+    $this->permissions = $permissions;
+    $this->roles = $roles;
+    $this->sanitizer = $sanitizer;
+    $this->session = $session;
+    $this->templates = $templates;
+    $this->user = $user;
+    $this->users = $users;
+  }
 
-    public function call()
-    {
-        $req = $this->request = f8\HttpRequest::instance();
-        $this->response = new f8\HttpResponse();
-        $method = strtolower($req->method());
-        $ret = null;
+  public function call()
+  {
+    $req = $this->request = f8\HttpRequest::instance();
+    $this->response = new f8\HttpResponse();
+    $method = strtolower($req->method());
+    $ret = null;
 
-        try {
-            if (method_exists($this, $method)) {
-                $ret = $this->$method();
+    try {
+      if (method_exists($this, $method)) {
+        $ret = $this->$method();
 
-                // Just die on redirect
-                if ($this->response->is_redirect()) {
-                    die();
-                }
-
-                // Body set, just return the response
-                $body = $this->response->body();
-                if (!empty($body)) {
-                    return $this->response;
-                }
-
-                // Set created status on sucessfull post
-                if ($method == 'post' && $ret && $this->response->status() == 0) {
-                    $this->response->set_status(201); // CREATED
-                }
-            } elseif (method_exists($this, 'all')) {
-                $ret = $this->all();
-            } elseif ($method == 'get') {
-                $ret = $this->page_data();
-            } else {
-                throw new f8\HttpException(405, "Method '$method' not allowed for resource.");
-            }
-        } catch (f8\HttpException $ex) {
-            $this->response->set_status($ex->getStatusCode());
-            $this->response->set_header('Allow', implode(', ', $this->get_allowed()));
-            $this->response->set_body(json_encode(array('error' => $ex->getMessage())));
-        } catch (Exception $ex) {
-            $this->response->set_status(500);
-            $this->response->set_body(json_encode(array('error' => $ex->getMessage())));
-
-            return $this->response;
+        // Just die on redirect
+        if ($this->response->is_redirect()) {
+          die();
         }
 
-        // Set NO CONTENT on blank responses
+        // Body set, just return the response
         $body = $this->response->body();
-        if (!$ret && empty($body)) {
-            if ($this->response->status() == 0) {
-                $this->response->set_status(204); // No content
-            }
-
-            return $this->response;
+        if (!empty($body)) {
+          return $this->response;
         }
 
-        if ($ret) {
-            // Serialize to JSON
-            if (is_array($ret) || is_object($ret)) {
-                $this->response->set_header('Content-Type', 'application/json');
-                $this->response->set_body(json_encode($ret));
-            } elseif (is_string($ret)) {
-                if (f8\Strings::starts_with(trim($ret), '<') && f8\Strings::ends_with(trim($ret), '>')) {
-                    $this->response->set_header('Content-Type', 'text/html');
-                } else {
-                    $this->response->set_header('Content-Type', 'text/plain');
-                }
-                $this->response->set_body($ret);
-            }
+        // Set created status on sucessfull post
+        if ($method == 'post' && $ret && $this->response->status() == 0) {
+          $this->response->set_status(201); // CREATED
+        }
+      } elseif (method_exists($this, 'all')) {
+        $ret = $this->all();
+      } elseif ($method == 'get') {
+        $ret = $this->page_data();
+      } else {
+        throw new f8\HttpException(405, "Method '$method' not allowed for resource.");
+      }
+    } catch (f8\HttpException $ex) {
+      $this->response->set_status($ex->getStatusCode());
+      $this->response->set_header('Allow', implode(', ', $this->get_allowed()));
+      $this->response->set_body(json_encode(array('error' => $ex->getMessage())));
+    } catch (Exception $ex) {
+      $this->response->set_status(500);
+      $this->response->set_body(json_encode(array('error' => $ex->getMessage())));
+
+      return $this->response;
+    }
+
+    // Set NO CONTENT on blank responses
+    $body = $this->response->body();
+    if (!$ret && empty($body)) {
+      if ($this->response->status() == 0) {
+        $this->response->set_status(204); // No content
+      }
+
+      return $this->response;
+    }
+
+    if ($ret) {
+      // Serialize to JSON
+      if (is_array($ret) || is_object($ret)) {
+        $this->response->set_header('Content-Type', 'application/json');
+        $this->response->set_body(json_encode($ret));
+      } elseif (is_string($ret)) {
+        if (f8\Strings::starts_with(trim($ret), '<') && f8\Strings::ends_with(trim($ret), '>')) {
+          $this->response->set_header('Content-Type', 'text/html');
+        } else {
+          $this->response->set_header('Content-Type', 'text/plain');
+        }
+        $this->response->set_body($ret);
+      }
+    }
+
+    return $this->response;
+  }
+
+  // Do nothing - override if required
+  public function before()
+  {
+  }
+  public function after()
+  {
+  }
+
+  public function helper($func)
+  {
+    if (!is_callable($func)) {
+      $func = array($this, $func);
+    }
+
+    View::add_helper($func);
+  }
+
+  // Get user settings
+  protected function setting($name)
+  {
+    static $settings = null;
+    if ($settings == null) {
+      $settings = wire('pages')->get('/settings/');
+    }
+
+    return $settings->$name;
+  }
+
+  // Get rendered partial
+  protected function partial($name, $data = array())
+  {
+    return $this->get_view()->partial($name, $data);
+  }
+
+  protected function get_view()
+  {
+    if (isset($this->view)) {
+      return $this->view;
+    }
+
+    $view = new View($this);
+    $view->set_base_path(f8\Paths::join($this->config->paths->templates, 'views'));
+    $view->set_asset_uri(f8\Paths::join($this->config->urls->templates, 'assets'));
+
+    return $this->view = $view;
+  }
+
+  protected function get_allowed()
+  {
+    return array_filter(f8\HttpRequest::$http_methods, function ($m) {
+      return $m == 'GET' || method_exists($this, strtolower($m));
+    });
+  }
+
+  protected function param($name)
+  {
+    if (!$this->request) {
+      return;
+    }
+
+    return $this->request->param($name);
+  }
+
+  protected function page_data()
+  {
+    $fields = func_get_args();
+    $page = &$this->page;
+    $outputFormatting = $page->outputFormatting;
+    $page->setOutputFormatting(false);
+
+    if (empty($fields)) {
+      $fields = array(
+        'id', 'parent_id', 'templates_id', 'name', 'status', 'sort',
+        'sortfield', 'numChildren', 'template', 'parent', 'data',
+      );
+    }
+
+    $data = array();
+    $has_data_field = false;
+    foreach ($fields as $f) {
+      switch ($f) {
+      case 'data':
+        $has_data_field = true;
+        break;
+      case 'template':
+        $data['template'] = $page->template->name;
+        break;
+      case 'parent':
+        $data['parent'] = $page->template->path;
+        break;
+      default:
+        $data[$f] = $page->$f;
+      }
+    }
+
+    if ($has_data_field) {
+      $data['data'] = array();
+      foreach ($page->template->fieldgroup as $field) {
+        if ($field->type instanceof FieldtypeFieldsetOpen) {
+          continue;
         }
 
-        return $this->response;
+        $value = $page->get($field->name);
+        $data['data'][$field->name] = $field->type->sleepValue($page, $field, $value);
+      }
     }
 
-    // Do nothing - override if required
-    public function before()
-    {
-    }
-    public function after()
-    {
-    }
+    $page->setOutputFormatting($outputFormatting);
 
-    public function helper($func)
-    {
-        if (!is_callable($func)) {
-            $func = array($this, $func);
-        }
-
-        View::add_helper($func);
-    }
-
-    // Get user settings
-    protected function setting($name)
-    {
-        static $settings = null;
-        if ($settings == null) {
-            $settings = wire('pages')->get('/settings/');
-        }
-
-        return $settings->$name;
-    }
-
-    // Get rendered partial
-    protected function partial($name, $data = array())
-    {
-        return $this->get_view()->partial($name, $data);
-    }
-
-    protected function get_view()
-    {
-        if (isset($this->view)) {
-            return $this->view;
-        }
-
-        $view = new View($this);
-        $view->set_base_path(f8\Paths::join($this->config->paths->templates, 'views'));
-        $view->set_asset_uri(f8\Paths::join($this->config->urls->templates, 'assets'));
-
-        return $this->view = $view;
-    }
-
-    protected function get_allowed()
-    {
-        return array_filter(f8\HttpRequest::$http_methods, function ($m) {
-            return $m == 'GET' || method_exists($this, strtolower($m));
-        });
-    }
-
-    protected function param($name)
-    {
-        if (!$this->request) {
-            return;
-        }
-
-        return $this->request->param($name);
-    }
-
-    protected function page_data()
-    {
-        $fields = func_get_args();
-        $page = &$this->page;
-        $outputFormatting = $page->outputFormatting;
-        $page->setOutputFormatting(false);
-
-        if (empty($fields)) {
-            $fields = array(
-                'id', 'parent_id', 'templates_id', 'name', 'status', 'sort',
-                'sortfield', 'numChildren', 'template', 'parent', 'data',
-            );
-        }
-
-        $data = array();
-        $has_data_field = false;
-        foreach ($fields as $f) {
-            switch ($f) {
-            case 'data':
-                $has_data_field = true;
-                break;
-            case 'template':
-                $data['template'] = $page->template->name;
-                break;
-            case 'parent':
-                $data['parent'] = $page->template->path;
-                break;
-            default:
-                $data[$f] = $page->$f;
-            }
-        }
-
-        if ($has_data_field) {
-            $data['data'] = array();
-            foreach ($page->template->fieldgroup as $field) {
-                if ($field->type instanceof FieldtypeFieldsetOpen) {
-                    continue;
-                }
-
-                $value = $page->get($field->name);
-                $data['data'][$field->name] = $field->type->sleepValue($page, $field, $value);
-            }
-        }
-
-        $page->setOutputFormatting($outputFormatting);
-
-        return $data;
-    }
+    return $data;
+  }
 }
--- core/HttpRequest.php
+++ core/HttpRequest.php
@@ -16,106 +16,110 @@ namespace fixate;
  */
 class HttpRequest
 {
-    public static $http_methods = array(
-        'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'COPY',
-        'HEAD', 'OPTIONS', 'LINK', 'UNLINK', 'PURGE',
-    );
+  public static $http_methods = array(
+    'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'COPY',
+    'HEAD', 'OPTIONS', 'LINK', 'UNLINK', 'PURGE',
+  );
 
-    public static function instance()
-    {
-        static $instance = null;
-        if ($instance == null) {
-            $instance = new static();
-        }
-
-        return $instance;
+  public static function instance()
+  {
+    static $instance = null;
+    if ($instance == null) {
+      $instance = new static();
     }
 
-    protected $headers = null;
-    protected $_params = null;
+    return $instance;
+  }
 
-    public function headers()
-    {
-        if ($headers) {
-            return $headers;
-        }
+  protected $headers = null;
+  protected $_params = null;
 
-        return $headers = $this->get_all_headers();
+  public function headers()
+  {
+    if ($headers) {
+      return $headers;
     }
 
-    public function method()
-    {
-        return strtoupper($_SERVER['REQUEST_METHOD']);
+    return $headers = $this->get_all_headers();
+  }
+
+  public function method()
+  {
+    return strtoupper($_SERVER['REQUEST_METHOD']);
+  }
+
+  public function header($name)
+  {
+    $headers = $this->headers();
+    if (array_key_exists($name, $headers)) {
+      return $headers[$name];
     }
 
-    public function header($name)
-    {
-        $headers = $this->headers();
-        if (array_key_exists($name, $headers)) {
-            return $headers[$name];
-        }
+    return;
+  }
 
-        return;
+  public function path() {
+    return $_SERVER['REQUEST_URI'];
+  }
+
+  public function params()
+  {
+    if ($this->_params) {
+      return $this->_params;
     }
 
-    public function params()
-    {
-        if ($this->_params) {
-            return $this->_params;
-        }
+    return $this->_params = $this->get_all_params();
+  }
 
-        return $this->_params = $this->get_all_params();
+  public function param($name, $default = false)
+  {
+    $params = $this->params();
+    if (array_key_exists($name, $params)) {
+      return $params[$name];
     }
 
-    public function param($name)
-    {
-        $params = $this->params();
-        if (array_key_exists($name, $params)) {
-            return $params[$name];
-        }
+    return $default;
+  }
 
-        return;
+  protected function get_all_params()
+  {
+    switch ($this->method()) {
+    case 'GET':
+    case 'COPY':
+    case 'HEAD':
+    case 'OPTIONS':
+    case 'PURGE':
+      return $_GET;
+    case 'PUT':
+    case 'DELETE':
+      $vars = array();
+      parse_str(file_get_contents('php://input'), $vars);
+
+      return $vars;
+    case 'POST':
+    case 'PATCH':
+    case 'LINK':
+    case 'UNLINK':
+      return array_merge($_GET, $_POST);
+    default:
+      return array();
+    }
+  }
+
+  protected function get_all_headers()
+  {
+    $headers = '';
+    foreach ($_SERVER as $name => $value) {
+      if (substr($name, 0, 5) == 'HTTP_') {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
     }
 
-    protected function get_all_params()
-    {
-        switch ($this->method()) {
-        case 'GET':
-        case 'COPY':
-        case 'HEAD':
-        case 'OPTIONS':
-        case 'PURGE':
-            return $_GET;
-        case 'PUT':
-        case 'DELETE':
-            $vars = array();
-            parse_str(file_get_contents('php://input'), $vars);
+    return $headers;
+  }
 
-            return $vars;
-        case 'POST':
-        case 'PATCH':
-        case 'LINK':
-        case 'UNLINK':
-            return array_merge($_GET, $_POST);
-        default:
-            return array();
-        }
-    }
-
-    protected function get_all_headers()
-    {
-        $headers = '';
-        foreach ($_SERVER as $name => $value) {
-            if (substr($name, 0, 5) == 'HTTP_') {
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-            }
-        }
-
-        return $headers;
-    }
-
-    private function __clone()
-    {
-        trigger_error('Clone disabled for singleton class.', E_ERROR);
-    }
+  private function __clone()
+  {
+    trigger_error('Clone disabled for singleton class.', E_ERROR);
+  }
 }
