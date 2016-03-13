992e826b2412ad534c1657a2121cf62273ba4ed3
Larry Botha <larry@fixate.it>
2016-03-13 23:31:58 +0200

    wrap form in id and submit to page with hash

--- views/partials/contact_form.php
+++ views/partials/contact_form.php
@@ -1,23 +1,25 @@
-<style type="text/css">.hp-model{ display: none;}</style>
-<?php if ($has_sent) : ?>
-  Thank you for contacting us, we will be in touch shortly.
-<?php endif ?>
+<div id="contact">
+  <style type="text/css">.hp-model{ display: none;}</style>
+  <?php if ($has_sent) : ?>
+    Thank you for contacting us, we will be in touch shortly.
+  <?php endif ?>
 
-<form action="./" method="post" id="contact-form" name="contact-form">
-  <label for="name">Name</label>
-  <input type="text" name="name" required value="<?= $form['name']['value'] ?>">
-  <?= $form['name']['error'] ?>
+  <form action="./#contact" method="post" id="contact-form" name="contact-form">
+    <label for="name">Name</label>
+    <input type="text" name="name" required value="<?= $form['name']['value'] ?>">
+    <?= $form['name']['error'] ?>
 
-  <label for="email">Email</label>
-  <input type="email" name="email" required value="<?= $form['email']['value'] ?>">
-  <?= $form['email']['error'] ?>
+    <label for="email">Email</label>
+    <input type="email" name="email" required value="<?= $form['email']['value'] ?>">
+    <?= $form['email']['error'] ?>
 
-  <label for="message">Message</label>
-  <textarea name="message" required rows=6><?= $form['message']['value'] ?></textarea>
-  <?= $form['message']['error'] ?>
+    <label for="message">Message</label>
+    <textarea name="message" required rows=6><?= $form['message']['value'] ?></textarea>
+    <?= $form['message']['error'] ?>
 
-  <?php // honeypot ?>
-  <input class="hp-model" type="text" name="model" value="">
+    <?php // honeypot ?>
+    <input class="hp-model" type="text" name="model" value="">
 
-  <button type="submit" name="submit" value="submit">submit</button>
-</form>
+    <button type="submit" name="submit" value="submit">submit</button>
+  </form>
+</div>
