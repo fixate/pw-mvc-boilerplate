71797437009bc42040621b702e51710b59bdea9b
Larry Botha <larry@fixate.it>
2017-03-13 11:56:22 +0200

    add svg inline icon partial

---
+++ views/partials/inline-icon.html.php
@@ -0,0 +1,7 @@
+<i class="<?= isset($icon_classname) ? $icon_classname : 'icon' ?>">
+  <i class="<? = isset($icon_inner_classname) ? $icon_inner_classname : 'icon__inner' ?>">
+    <svg>
+      <use xlink:href="#icon-<?= isset($icon_name) ? $icon_name : '' ?>" />
+    </svg>
+  </i>
+</i>
