9231121a9b8642a146f5bbef23907dee1e406294
Larry Botha <larry@fixate.it>
2017-03-14 08:46:26 +0200

    improve partial variables

--- views/partials/inline-icon.html.php
+++ views/partials/inline-icon.html.php
@@ -1,7 +1,7 @@
-<i class="<?= isset($icon_classname) ? $icon_classname : 'icon' ?>">
-  <i class="<?= isset($icon_inner_classname) ? $icon_inner_classname : 'icon__inner' ?>">
+<i class="<?= isset($classname) ? $classname : 'icon' ?>">
+  <i class="<?= isset($classname) ? $classname : 'icon__inner' ?>">
     <svg>
-      <use xlink:href="#icon-<?= isset($icon_name) ? $icon_name : '' ?>" />
+      <use xlink:href="#icon-<?= isset($id) ? $id : '' ?>" />
     </svg>
   </i>
 </i>
