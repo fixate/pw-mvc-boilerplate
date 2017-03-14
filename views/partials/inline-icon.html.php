8ab40716f9efafe4c24af1ac40e336c462694316
Larry Botha <larry@fixate.it>
2017-03-14 08:47:21 +0200

    fix typo

--- views/partials/inline-icon.html.php
+++ views/partials/inline-icon.html.php
@@ -1,5 +1,5 @@
 <i class="<?= isset($classname) ? $classname : 'icon' ?>">
-  <i class="<?= isset($classname) ? $classname : 'icon__inner' ?>">
+  <i class="<?= isset($classname_inner) ? $classname_inner : 'icon__inner' ?>">
     <svg>
       <use xlink:href="#icon-<?= isset($id) ? $id : '' ?>" />
     </svg>
