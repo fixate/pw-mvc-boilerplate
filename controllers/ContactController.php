b58e4abb5b379354e3cba854243cc162c4b254ba
Larry Botha <larry@fixate.it>
2017-03-20 16:11:38 +0200

    improve contact controller

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -6,84 +6,91 @@
  */
 class ContactController extends ApplicationController
 {
-    public function index()
-    {
-        return $this->render(array(
-            'form' => $this->getContactForm(),
-        ));
+  public function index()
+  {
+    return $this->render(array(
+      'form' => $this->get_contact_form(),
+    ));
+  }
+
+  protected function get_contact_form()
+  {
+    $can_send = false;
+    $form_has_errors = false;
+    $input = $this->input;
+    $honeypot_filled = !!$input->post->model; // this is a deliberately misleading name
+    $is_submission = !!$input->post->submit;
+    $sanitizer = $this->sanitizer;
+    $view = new View($this);
+
+    $submission = array(
+      'name' => array(
+        'value' => $sanitizer->text($input->name),
+        'error' => $is_submission ? $this->formValidateRequiredField($input->name, 'Please fill in your name') : '',
+      ),
+      'email' => array(
+        'value' => $sanitizer->email($input->email),
+        'error' => $is_submission ? $this->formValidateRequiredEmailField($input->email, 'Please fill in your email address') : '',
+      ),
+      'message' => array(
+        'value' => $sanitizer->textarea($input->message),
+        'error' => $is_submission ? $this->formValidateRequiredField($input->message, 'Please add a message'): '',
+      ),
+    );
+
+    $form_has_errors = $this->formHasErrors($submission);
+    $can_send = $is_submission && !$form_has_errors && !$honeypot_filled;
+
+    if ($can_send) {
+      $this->handle_contact_submission($submission);
+      $this->create_submission_page($submission);
     }
 
-    protected function getContactForm()
-    {
-        $can_send = false;
-        $form_has_errors = false;
-        $input = $this->input;
-        $honeypot_filled = !!$input->post->model; // this is a deliberately misleading name
-        $is_submission = !!$input->post->submit;
-        $sanitizer = $this->sanitizer;
-        $view = new View($this);
+    return $view->partial(
+      'forms/contact',
+      array('form' => $submission, 'has_sent' => $can_send)
+    );
+  }
 
-        $submission = array(
-          'name' => array(
-            'value' => $sanitizer->text($input->name),
-            'error' => $is_submission ? $this->formValidateRequiredField($input->name, 'Please fill in your name') : '',
-          ),
-          'email' => array(
-            'value' => $sanitizer->email($input->email),
-            'error' => $is_submission ? $this->formValidateRequiredEmailField($input->email, 'Please fill in your email address') : '',
-          ),
-          'message' => array(
-            'value' => $sanitizer->textarea($input->message),
-            'error' => $is_submission ? $this->formValidateRequiredField($input->message, 'Please add a message'): '',
-          ),
-        );
+  protected function handle_contact_submission($submission)
+  {
+    $pages = $this->pages;
+    $site_name = $pages->get('/settings')->site_name;
+    $admin_email = $pages->get('template=contact')->email;
+    extract($submission);
 
-        $form_has_errors = $this->formHasErrors($submission);
-        $can_send = $is_submission && !$form_has_errors && !$honeypot_filled;
+    $admin_template = './views/email/contact/to_admin.html.php';
+    $submitter_template = './views/email/contact/to_submitter.html.php';
 
-        if ($can_send) {
-          $this->handleContactSubmission($submission);
-        }
+    $admin_fields = array(
+      'email_from' => $email['value'],
+      'email_to' => $email['value'],
+      'subject' => "{$site_name} - Contact Query",
+      'name' => $name['value'],
+      'message' => $message['value'],
+    );
 
-        return $view->partial(
-          'contact_form',
-          array('form' => $submission, 'has_sent' => $can_send)
-        );
-    }
+    $submitter_fields = array(
+      'email_from' => $email['value'],
+      'email_to' => $email['value'],
+      'subject' => "{$site_name} - Thank you for your message",
+      'name' => $name['value'],
+      'message' => $message['value'],
+    );
 
-    protected function handleContactSubmission($submission)
-    {
-        $pages = $this->pages;
-        $site_name = $pages->get('/settings')->site_name;
-        extract($submission);
+    $this->formSendEmail($admin_fields, $admin_template);
+    $this->formSendEmail($submitter_fields, $submitter_template);
+  }
 
-        $templates = array(
-            'to_admin' => './views/email/contact_form_to_admin.html.php',
-            'to_submitter' => './views/email/contact_form_to_submitter.html.php',
-        );
-
-        $fields = array(
-            'admin_email' => $pages->get('/contact')->admin_email,
-            'admin_subject' => "{$site_name} - Contact Query",
-            'submitter_subject' => "{$site_name} - Thank you for your message",
-            'submitter_name' => $name['value'],
-            'submitter_email' => $email['value'],
-            'submitter_message' => $message['value'],
-        );
-
-        $this->formSendEmails($fields, $templates);
-        $this->saveContactSubmission($fields, $templates);
-    }
-
-    private function saveContactSubmission(array $fields)
-    {
-        $newpage = new Page();
-        $newpage->template = 'contact_submission';
-        $newpage->title = "{$fields['submitter_name']} - " . date('[H:i d M Y]');
-        $newpage->email = $fields['submitter_email'];
-        $newpage->body = $fields['submitter_message'];
-        $newpage->setOutputFormatting(true);
-        $newpage->parent_id = $this->page->id;
-        $newpage->save();
-    }
+  private function create_submission_page(array $fields)
+  {
+    $newpage = new Page();
+    $newpage->template = 'contact_submission';
+    $newpage->title = "{$fields['name']['value']} - " . date('[H:i d M Y]');
+    $newpage->email = $fields['email']['value'];
+    $newpage->body = $fields['message']['value'];
+    $newpage->setOutputFormatting(true);
+    $newpage->parent_id = $this->page->id;
+    $newpage->save();
+  }
 }
