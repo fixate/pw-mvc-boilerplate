a4aac5b5d9be49657aa374180174fd1b3b8ad6ee
Larry Botha <larry@fixate.it>
2019-03-05 15:49:55 +0200

    remove deprecated function

--- core/Utils.php
+++ core/Utils.php
@@ -9,7 +9,7 @@ class Strings
         if ($first_capital) {
             $str[0] = strtoupper($str[0]);
         }
-        $func = create_function('$c', 'return strtoupper($c[1]);');
+        $func = function($c) { return strtoupper($c[1]);};
 
         return preg_replace_callback('/_([a-z])/', $func, $str);
     }
