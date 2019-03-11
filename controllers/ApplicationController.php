f900d6f1008a8ace456768a0329f580ba764f60a
Larry Botha <larry@fixate.it>
2019-03-11 12:39:00 +0200

    remove jquery from cdn urls

--- controllers/ApplicationController.php
+++ controllers/ApplicationController.php
@@ -35,11 +35,11 @@ class ApplicationController extends Controller
         VideoEmbed::__vidembedInitialize($this);
         Utils::__utilsInitialize($this);
 
-        $this->js_add_cdn(
-            '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
-            'window.jQuery',
-            'vendor/jquery/dist/jquery.js'
-        );
+        // $this->js_add_cdn(
+        //     'cdn-some-lib-url', // cdn URL
+        //     'window["some-lib"]', // object to assign to window
+        //     'vendor/jquery/dist/some-lib.js' // local fallback
+        // );
     }
 
     // Fallback index
