0d4085d9ecb8be017e3cc61df8c2e786c59dc43a
Larry Botha <larry@fixate.it>
2020-03-11 13:21:14 -0500

    improve conditions for outputting analytics

--- config/environment.php
+++ config/environment.php
@@ -4,17 +4,17 @@
 /*------------------------------------*\
     USER ENVIRONMENT
 \*------------------------------------*/
-$gtm_container_id = $pages->get('/settings')->gtm_container_id ? $pages->get('/settings')->gtm_container_id : false;
-$ga_uacode = $pages->get('/settings')->ga_uacode ? $pages->get('/settings')->ga_uacode : false;
-$ga_wmt_meta = $pages->get('/settings')->ga_wmt_meta ? $pages->get('/settings')->ga_wmt_meta : false;
+$gtm_container_id = $pages->get('/settings')->gtm_container_id;
+$ga_uacode = $pages->get('/settings')->ga_uacode;
+$ga_wmt_meta = $pages->get('/settings')->ga_wmt_meta;
 
 $environment = array(
-    // 'env' => 'production',
+  // 'env' => 'production',
 
-    // Google analytics
-    'gtm_container_id' => $gtm_container_id,
-    'ga_uacode' => $ga_uacode,
-    'ga_wmt_meta' => $ga_wmt_meta,
-    // Asset Manifest
-    'use_manifest' => true,
+  // Google analytics
+  'gtm_container_id' => $gtm_container_id,
+  'ga_uacode' => $ga_uacode,
+  'ga_wmt_meta' => $ga_wmt_meta,
+  // Asset Manifest
+  'use_manifest' => true,
 );
--- views/partials/analytics.html.php
+++ views/partials/analytics.html.php
@@ -1,13 +1,13 @@
 <?php if ($env->is_production) : ?>
-  <?php if ($env->gtm_container_id !== false) : ?>
+  <?php if ($env->gtm_container_id) : ?>
     <?= $this->partial('analytics/google_tag_manager_head') ?>
   <?php endif ?>
 
-  <?php if ($env->ga_uacode !== false) : ?>
+  <?php if ($env->ga_uacode && !$env->gtm_container_id) : ?>
     <?= $this->partial('analytics/google_analytics') ?>
   <?php endif ?>
 
-  <?php if ($env->ga_wmt_meta !== false) : ?>
+  <?php if ($env->ga_wmt_meta && !$env->gtm_container_id) : ?>
     <?= $this->partial('analytics/google_webmaster_tools') ?>
   <?php endif ?>
 <?php endif ?>
